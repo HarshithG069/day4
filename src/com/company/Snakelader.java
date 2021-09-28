04805881702fd1d1438f333a98deb5a68d40dce6
HarshithaG069 <harshithag069@gmail.com>
2021-09-28 21:01:33 +0530

    day4

--- src/com/company/Snakelader.java
+++ src/com/company/Snakelader.java
@@ -1,24 +1,48 @@
 package com.company;
 
 public class Snakelader {
+    public static void main(String[] args) {
+        int c = 0;
+        int d = 0;
+        int k = 0;
+        while (d <= 99 && c <= 99) {
+            int Player1 = (int) Math.floor(Math.random() * 10) % 6;
+            int Player2 = (int) Math.floor(Math.random() * 10) % 6;
+            k = k + 1;
+            int b = (int) Math.floor(Math.random() * 10) % 3;
+            switch (b) {
+                case 1:
+                    c = c - Player1;
+                    d = d - Player2;
+                    if (c < 0) {
+                        c = 0;
+                    }
+                    if (d < 0) {
+                        d = 0;
+                    }
 
+                    break;
+                case 2:
+                    c = c + Player1;
+                    d = d + Player2;
+                    if (c > 100) {
+                        c = c - Player1;
+                    }
+                    if (d > 100) {
+                        d = d - Player2;
+                    }
+                    break;
+                default:
+                    c = c + 0;
+                    d = d + 0;
+            }
+        }
+        if (c == 100) {
+            System.out.println("Player1 won the game by " + k + " time throwing the dice");
+        } else {
+            System.out.println("Player2 won the game by " + k + " time throwing the dice");
 
-    private int position = 0;
-    private String name = null;
+        }
 
-    public Snakelader(String name) {
-        this.name = name;
-    }
-    public int getPosition() {
-        return position;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setPosition(int currentPosition) {
-        this.position = currentPosition;
-    }
-    public void setName(String playerName) {
-        this.name = playerName;
     }
 }
